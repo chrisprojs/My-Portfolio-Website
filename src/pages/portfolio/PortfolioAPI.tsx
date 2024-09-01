2e86711f692f7ca5721c5fde524f19c9c645ddbc
chrisprojs <christiananggaresta20@gmail.com>
2024-09-01 14:07:36 +0700

    Add: add some portfolio

--- src/pages/portfolio/PortfolioAPI.tsx
+++ src/pages/portfolio/PortfolioAPI.tsx
@@ -21,12 +21,24 @@ async function retryFetch(url: string, delay: number = 1000): Promise<Response> 
   throw new Error('Failed to fetch data after multiple attempts');
 }
 
+const customSortOrder: Record<string, number> = {
+  "BINUSMAYA LMS Website": 1,
+  "My Portfolio Website": 2,
+  "Eksotika Prima Rub Oil Supplier Website": 3
+};
+
+function customSort(a: Project, b: Project): number {
+  const aOrder = customSortOrder[a.title] || Infinity;
+  const bOrder = customSortOrder[b.title] || Infinity;
+  return aOrder - bOrder;
+}
+
 // Function to fetch and transform project data
 export async function getAllProjects(): Promise<Project[]> {
   try {
     const response = await retryFetch(`${apiUrl}/api/Projects`);
     const data = await response.json();
-    return data.map((project: { projectId: any; title: any; details: any; skills: any[]; images: any[]; publicationLink: any; authors: any[]; likes: any; }) => ({
+    const transformedData = data.map((project: { projectId: any; title: any; details: any; skills: any[]; images: any[]; publicationLink: any; authors: any[]; likes: any; }) => ({
       projectId: project.projectId,
       title: project.title,
       details: project.details,
@@ -39,6 +51,9 @@ export async function getAllProjects(): Promise<Project[]> {
       authors: project.authors.map((author: { author: any; }) => author.author),
       likes: project.likes
     }));
+    
+    // Sort the transformed data
+    return transformedData.sort(customSort)
   } catch (error) {
     console.error('Error fetching projects:', error);
     return []; // Return an empty array in case of error
