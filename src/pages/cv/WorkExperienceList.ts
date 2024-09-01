4c84383814c86affa1410d164f5098039bb3a13b
chrisprojs <christiananggaresta20@gmail.com>
2024-09-01 17:05:24 +0700

    Add: CV Work Experience added

--- src/pages/cv/WorkExperienceList.ts
+++ src/pages/cv/WorkExperienceList.ts
@@ -1,13 +1,24 @@
 export const WorkExperienceList = [
   {
     company: "Bina Nusantara IT Division",
-    position: "Associate Member",
+    position: "Web Developer",
     duration: "March 2023 - February 2024",
     responsibilities: [
       `Shaped user experience for Binus University's digital platforms using React.js and Typescript.`,
       `Built responsive web interfaces for Learning Management System in Binus University.`,
       `Enhanced the performance of web applications by implementing efficient coding practices and optimizing resource usage.`,
       `Worked closely with the design and backend teams to ensure seamless integration of user interfaces with server-side logic.`,
+    ],
+  },
+  {
+    company: "Eksotika Prima",
+    position: "Web Developer and Marketer",
+    duration: "August 2024 - Present",
+    responsibilities: [
+      `Developed and maintained a rub oil supplier website, showcasing products sourced from Makassar.`,
+      `Managed digital marketing campaigns, focusing on targeted promotions near Danau Sunter and surrounding area.`,
+      `Successfully converted promotional efforts into a sale, securing a customer through strategic marketing initiatives.`,
+      `Helping family business by making promotional website for company`,
     ],
   },
 ];
